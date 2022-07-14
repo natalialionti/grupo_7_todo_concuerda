1fd029c4ec76b5bba869f723301c7f5a4ce3bc99
“mi <nataliasaxo@gmail.com>
2022-07-14 10:29:23 -0300

    estructura Node.js+Express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+const port = 3030; 
+
+app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
+   
+app.use(express.static("public"));  
