675f420887579156177df4305ce12fa12b59796e
MarFerreyra1 <ferreyramariana1994@gmail.com>
2022-07-24 21:38:31 -0300

    codigo para home

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require ("path");
 const port = 3030; 
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"));});
    
 app.use(express.static("public"));  
