cd56f41a7b96dd4072d825b39c18d3c6497d926b
RobertoCodino <codinodeian2008@gmail.com>
2022-07-25 10:09:18 -0300

    creo rama

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const port = 3030;
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "detalle_producto.html"));});
    
-app.use(express.static("public"));  
+app.use(express.static("public"));
