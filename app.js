4e5c1f20c8517c31ad2c8896f9ac9d2ccd7322c5
marcossanhu <marcossanhu@gmail.com>
2022-07-29 07:05:20 -0300

    register y login

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require ("path");
 const port = 3030; 
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
    
 app.use(express.static("public"));  
