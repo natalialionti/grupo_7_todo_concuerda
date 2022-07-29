c28e76daa67b212ef65019fccd95fdf682c83b8c
MarFerreyra1 <ferreyramariana1994@gmail.com>
2022-07-29 16:13:23 -0300

    home html y css

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const port = 3030;
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"));});
+app.get("productDetail", (req,res)=> {res.sendFile(path.join(__dirname,"views", "productDetail.html"))})
    
 app.use(express.static("public"));  
