59686a7aef18fca6a71e8dd825a54806acec9bb4
“natalialionti” <nataliasaxo@gmail.com>
2022-07-31 23:03:51 -0300

    ..

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const path = require ("path");
 const port = 3030; 
 
 app.listen (port, () => console.log("Servidor corriendo en puerto " + port));
-<<<<<<< HEAD
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "home.html"));});
 app.get('/carrito', (req, res) => {res.sendFile(path.join(__dirname, "views", "carrito.html"));});
-=======
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"));});
-app.get("/product-detail", (req,res)=> {res.sendFile(path.join(__dirname,"views", "productDetail.html"))})
->>>>>>> mariana
    
 app.use(express.static("public"));  
